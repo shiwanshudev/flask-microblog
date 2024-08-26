798135c2ac9884e84437326abc5b1b579bdf6cb4
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-08-26 16:34:00 +0530

    Learn about template basics

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,36 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
     user = {'username': 'Ram Kumar'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    posts = [
+        {
+            'author': {'username': 'Leo'},
+            'body': 'It is a beautiful day!'
+        },
+        {
+            'author': {'username': 'Mia'},
+            'body': 'Just finished a great workout!'
+        },
+        {
+            'author': {'username': 'Noah'},
+            'body': 'Loving this new book I started.'
+        },
+        {
+            'author': {'username': 'Ava'},
+            'body': 'Had an amazing coffee this morning!'
+        },
+        {
+            'author': {'username': 'Ethan'},
+            'body': 'Excited for the weekend plans!'
+        },
+        {
+            'author': {'username': 'Sophia'},
+            'body': 'Enjoying a peaceful evening walk.'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
