20da2f456d3884a6e5e615abb17b03e1da88551d
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-08-27 20:29:26 +0530

    Add base template and form

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'username': 'Ram Kumar'}
     posts = [
@@ -34,3 +34,8 @@ def index():
     ]
 
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Sign In', form=form)
