31313255b9a94e86bcc5c9f5173fd1c304439a07
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-09-01 19:14:10 +0530

    Modify login route and add flash message

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
-from flask import render_template
 from app.forms import LoginForm
+from flask import render_template, flash, redirect
 
 @app.route('/')
 @app.route('/index')
@@ -35,7 +35,12 @@ def index():
 
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
     return render_template('login.html',title='Sign In', form=form)
